6b76210d29486cb542371bbb16872ac3cb2f1afc
Pavel Perov <pavelperov93@gmail.com>
2019-10-27 17:39:41 +0200

    update

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ const form = document.querySelector('form');
 const input = document.querySelector('input');
 const imageSection = document.querySelector('.images');
 let page = 1;
-const API_URL = `https://api.unsplash.com/search/photos?page=${page}&per_page=9&client_id=${API_CLIENTID}`
+const API_URL = `https://api.unsplash.com/search/photos?page=${page}&per_page=10&client_id=${API_CLIENTID}`
+
 
 
 form.addEventListener('submit', formSubmitted);
@@ -16,16 +17,17 @@ function formSubmitted(event) {
   search(searchTerm)
     .then(displayImages)
 
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      var myObj = JSON.parse(this.responseText);
-      document.getElementById("totalPages").innerHTML = 'Total Pages: ' + myObj.total_pages;
-    }
-  };
+    var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("totalPages").innerHTML = 'Total Pages: ' + myObj.total_pages;
+    document.getElementById("totalImages").innerHTML = 'Total Images: ' + myObj.total;
+  }
+};
 
-  xmlhttp.open("GET", `${API_URL}&query=${searchTerm}`, true);
-  xmlhttp.send();
+xmlhttp.open("GET", `${API_URL}&query=${searchTerm}`, true);
+xmlhttp.send();
 }
 
 function searchStart() {
@@ -34,6 +36,14 @@ function searchStart() {
 
 function search(searchTerm) {
   let searchUrl = `${API_URL}&query=${searchTerm}`;
+
+// GET Query for getting Data from JSON
+//   $.getJSON(searchUrl, function(data) {
+//     console.log(data.results[0].user);
+    
+//  });
+
+
   console.log(searchUrl)
   return fetch(searchUrl)
     .then(response => response.json())
@@ -53,14 +63,14 @@ function nextPage(event) {
   searchStart();
   next(searchTerm)
     .then(displayImages);
-  page++;
-  console.log(page);
-  document.getElementById("currentPage").innerHTML = '| Current Page: ' + page;
+    page++;
+    console.log(page);
+    document.getElementById("currentPage").innerHTML = '| Current Page: ' + page;
 
 }
 
 function next(searchTerm) {
-  let url = `https://api.unsplash.com/search/photos?page=${page + 1}&per_page=9&client_id=${API_CLIENTID}&query=${searchTerm}`
+  let url = `https://api.unsplash.com/search/photos?page=${page + 1}&per_page=10&client_id=${API_CLIENTID}&query=${searchTerm}`
   console.log(url)
   return fetch(url)
     .then(response => response.json())
@@ -79,22 +89,22 @@ function prevPage(event) {
   searchStart();
   prev(searchTerm)
     .then(displayImages);
-  page--;
-  console.log(page);
+    page--;
+    console.log(page);
+    
+    
 
-
-
-  if (page == 0) {
-    $('#previous').attr('disabled', 'disabled');
-    document.getElementById("currentPage").innerHTML = ' ';
-  } else {
-    document.getElementById("currentPage").innerHTML = ' | Current Page: ' + page;
-  }
-
+    if(page == 0){
+      $('#previous').attr('disabled','disabled');
+      document.getElementById("currentPage").innerHTML = ' ';
+    } else {
+      document.getElementById("currentPage").innerHTML = ' | Current Page: ' + page;
+    }
+    
 }
 
 function prev(searchTerm) {
-  let url = `https://api.unsplash.com/search/photos?page=${page - 1}&per_page=9&client_id=${API_CLIENTID}&query=${searchTerm}`
+  let url = `https://api.unsplash.com/search/photos?page=${page - 1}&per_page=10&client_id=${API_CLIENTID}&query=${searchTerm}`
   console.log(url)
   return fetch(url)
     .then(response => response.json())
@@ -116,24 +126,9 @@ function displayImages(images) {
     <a href="${image.user.name}" target="_blank" class="user_link">Photo by: ${image.user.name}</a>`;
     imageSection.appendChild(imageContainer);
     console.log(`${image.user.name}`);
-
+    
   });
 }
 
 
 
-
-
-// Backup for show user links as <a>
-
-// function displayImages(images) {
-//   images.forEach(image => {
-//     let imageContainer = document.createElement('div');
-//     imageContainer.className = 'ImageResult'
-//     imageContainer.innerHTML = `<img src="${image.urls.regular}">
-// <a href="${image.links.html}" target="_blank" class="view_link">View on Unsplash</a>
-// <a href="${user.links.html}" target="_blank" class="user_link">Photo by: ${image.user.name}</a>`;
-//     imageSection.appendChild(imageContainer);
-
-//   });
-// }
